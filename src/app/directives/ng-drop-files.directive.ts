814a993165edefde36d7b4c3298bb09c4a406bb9
Nelly Fragoso Muñoz <nellyfrgoso1003gmail.com>
2020-11-27 23:48:36 -0600

    Validaciones desde la directiva

--- src/app/directives/ng-drop-files.directive.ts
+++ src/app/directives/ng-drop-files.directive.ts
@@ -1,11 +1,14 @@
 import { Directive, EventEmitter,ElementRef,
                     HostListener, Input, Output } from '@angular/core';
+import { FileItem } from '../models/file-item';
+
 
 @Directive({
   selector: '[appNgDropFiles]'
 })
 export class NgDropFilesDirective {
 
+  @Input() archivos: FileItem[] = [];
   @Output() mouseSobre: EventEmitter<boolean> = new EventEmitter();
 
   constructor() { }
@@ -20,6 +23,36 @@ export class NgDropFilesDirective {
   public onDragLeave( event: any ) {
     this.mouseSobre.emit(true);
 
+  }
+
+  //Validaciones
+  private _archivoPuedeSerCargado( archivo: File): boolean{
+    if ( !this._archivoYaFueDropeado(archivo.name ) && this._esimagen (archivo.type) ) {
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+
+  private _prevenirdetener(event ){
+      event._prevenirdetener();
+      event.stoppropagation();
+  }
+
+  private _archivoYaFueDropeado( nombreArchivo: string): boolean{
+    for ( const archivo of this.archivos){
+      if ( archivo.nombreArchivo == nombreArchivo ){
+        console.log('El archivo' + nombreArchivo + 'ya fue agregado');
+        return true;
+
+      }
+    }
+    return false;
+  }
+
+  private _esimagen( tipoArchivo: string): boolean{
+    return (tipoArchivo === '' || tipoArchivo === undefined)? false: tipoArchivo.startsWith('image');
   }
 
 }
